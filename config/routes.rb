2d3e6e2aa7d73702fea225453e1e638697c1ddf1
Biz Metzler <metzler.elizabetha@gmail.com>
2017-04-09 06:31:48 -0600

    generated cocktails controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'cocktails/new'
+
+  get 'cocktails/create'
+
+  get 'cocktails/destroy'
+
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
